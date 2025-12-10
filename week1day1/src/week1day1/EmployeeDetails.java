316d9b1db534f628108b47d68f0e08d411ed0beb
Rprasannas <ping.rps555@gmail.com>
2025-12-10 15:56:15 +0530

    Add files via upload
    Week1day1

---
+++ week1day1/src/week1day1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package week1day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String name="John Doe";
+      short age=30;
+      float salary=55000.75f;
+      char gender='M';
+      boolean acceptTerms=true;
+      float rating=4.5f;
+      long mobileNumber=9876543210L;
+      int pinCode=56001;
+      byte luckyNumber=7;
+      System.out.println("Employee Name: " +name);
+      System.out.println("Age: " +age);
+      System.out.println("Salary: " +salary);
+      System.out.println("Gender: " +gender);
+      System.out.println("Accept Terms: " +acceptTerms);      
+      System.out.println("Rating: " +rating);      
+      System.out.println("Mobile Number: " +mobileNumber);      
+      System.out.println("Pincode: " +pinCode);      
+      System.out.println("Lucky Number: " +luckyNumber);      
+      
+    		  
+	}
+
+}
